0d5d502f51762e749d5d710c2b0e7b47b5a2fd08
luiskapp <80328112+luiskapp@users.noreply.github.com>
2022-01-24 20:38:33 -0300

    Començando o Projeto

--- PROJETO_HEROI.js
+++ PROJETO_HEROI.js
@@ -1,1 +1,2 @@
-console.log('hello World!!');
+console.log('Olá eu sou o POKEDEX seja bem vindo! Vamos testar seu conhecimentos relacionados a suas batalhas POKEMON! Após um dia interio procurando novos POKEMONS e batalhando com amigos e metres de ginásios, preciso que me responda a essas 5 perguntas para saber se é um verdadeiro mestre POKEMON! ');
+
