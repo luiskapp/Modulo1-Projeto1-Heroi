d8c074532cd541dbdfcb358b83fe02e51498ff93
luiskapp <80328112+luiskapp@users.noreply.github.com>
2022-01-24 23:15:14 -0300

    Perguntas prontas

--- PROJETO_HEROI.js
+++ PROJETO_HEROI.js
@@ -1,3 +1,19 @@
 var prompt = require('prompt-sync')();
-console.log('Olá eu sou o POKEDEX seja bem vindo! Vamos testar seu conhecimentos relacionados a suas capturas e batalhas POKEMON! Após um dia interio procurando novos POKEMONS e batalhando com amigos e metres de ginásios, preciso que me responda a essas 5 perguntas para saber se é um verdadeiro mestre POKEMON! ');
+
+console.log('Olá eu sou o POKEDEX seja bem vindo! Vamos testar seu conhecimentos sobre POKEMONS! Após um longo tempo em sua jornada procurando novos POKEMONS e batalhando com amigos e metres de ginásios, preciso que responda a essas 5 perguntas para saber se é um verdadeiro mestre POKEMON! ');
+
+console.log('---------------------------------')
+
+let question = prompt('Na sua jornada capturou um POKEMON tipo sombrio? ');
+let question1 = prompt('Viu um POKEMON raro? ')
+let question2 = prompt('Conseguiu alguma insgnia? ')
+let question3 = prompt('Você tem um Picachu? ')
+let question4 = prompt('Os POKEMONS terrestres tem vantagem sobre os POKEMONS eletricos? ')
+while (question,question1,question2,question3,question4){
+    question++;
+
+    if (question === "sim"|| question === "nao"){
+        continue;
+    }
+}
 
